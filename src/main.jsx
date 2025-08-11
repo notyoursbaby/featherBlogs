19fcd6db9ad320c5ea4e1aa2bf47ea0327c2dde7
notyoursbaby <167247131+notyoursbaby@users.noreply.github.com>
2025-08-12 00:56:01 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,141 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Provider } from 'react-redux'
+import store from './store/store.js'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { AuthLayout, Error, LogoutBtn, Profile } from './components/index.js'
+
+
+/** PAGES IMPORT */
+import Home from './pages/Home'
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
+import AllPosts from './pages/AllPosts.jsx'
+import AddPost from './pages/AddPost.jsx'
+import EditPost from './pages/EditPost.jsx'
+import Post from './pages/Post.jsx'
+import VerifyEmail from './components/VerifyEmail.jsx'
+import EmailVerifySent from './components/EmailVerifySent.jsx'
+import Support from './components/Support.jsx'
+
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/login',
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        )
+      },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayout authentication={false}>
+            <Signup />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/all-posts",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AllPosts />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/add-post",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AddPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/edit-post/:slug",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <EditPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: '/profile',
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <Profile />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: '/logout',
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <LogoutBtn />
+          </AuthLayout>
+        )
+      },
+      {
+        path: "/post/:slug",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <Post />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: '/verify-email',
+        element: (
+          <AuthLayout authentication={false}>
+          {" "}
+            <VerifyEmail />
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/verify-email/status',
+        element: (
+          <AuthLayout authentication={false}>
+          {" "}
+            <EmailVerifySent />
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/contact-us',
+        element: (
+          <Support />
+        ),
+      },
+      {
+        path: '*',
+        element: <Error />,
+      }
+    ],
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={routes} />
+    </Provider>
+  </React.StrictMode>,
+)
