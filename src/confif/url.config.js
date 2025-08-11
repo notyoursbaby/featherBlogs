e518d7b6d6ad39a4cb0ec0a3da03d5cef1f93859
notyoursbaby <167247131+notyoursbaby@users.noreply.github.com>
2025-08-12 00:46:58 +0530

    Create url.config.js

---
+++ src/confif/url.config.js
@@ -0,0 +1,11 @@
+const urlConfig = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteFeedbackCollectionId: String(import.meta.env.VITE_APPWRITE_FEEDBACK_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    appwriteApiKey: String(import.meta.env.VITE_APPWRITE_API_KEY)
+}
+
+export default urlConfig
